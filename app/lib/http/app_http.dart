9a45cc42fb1f1d604bcf05443b96a25d113ed3ba
Zero <1300326388@qq.com>
2023-11-12 22:06:48 +0800

    opt http

--- app/lib/http/app_http.dart
+++ app/lib/http/app_http.dart
@@ -4,8 +4,8 @@ final dio = Dio();
 
 /// 初始化 http
 void initHttp() {
-  // dio.options.baseUrl = "http://116.62.237.65:8080/";
-  dio.options.baseUrl = "http://192.168.1.6:8080/";
+  dio.options.baseUrl = "http://116.62.237.65:8080/";
+  // dio.options.baseUrl = "http://192.168.1.6:8080/";
   dio.options.contentType = Headers.textPlainContentType;
   dio.interceptors.add(LogInterceptor()); // Log 日志
 }
